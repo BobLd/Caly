f32dfd8bcb09f35d8c88eca56fa25c3f33bfe5fc
BobLd <38405645+BobLd@users.noreply.github.com>
2024-06-15 18:24:29 +0100

    Implement resize SplitView

--- Caly.Core/Views/MainView.axaml.cs
+++ Caly.Core/Views/MainView.axaml.cs
@@ -17,7 +17,10 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Shapes;
 using Avalonia.Input;
 using Avalonia.VisualTree;
 using Caly.Core.Services.Interfaces;
@@ -25,8 +28,11 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Caly.Core.Views
 {
+    [TemplatePart("PART_SplitView", typeof(SplitView))]
     public partial class MainView : UserControl
     {
+        private SplitView? _splitView;
+
         public MainView()
         {
             InitializeComponent();
@@ -88,11 +94,98 @@ namespace Caly.Core.Views
 
                 stackPanel.SetCurrentValue(WidthProperty, width);
 
-                foreach (var textBlock in stackPanel.GetVisualDescendants().OfType<TextBlock>())
+                foreach (TextBlock textBlock in stackPanel.GetVisualDescendants().OfType<TextBlock>())
                 {
                     textBlock.InvalidateMeasure();
                 }
             }
         }
+
+        #region Resize SplitView.Pane
+
+        private static readonly Cursor SizeWestEastCursor = new(StandardCursorType.SizeWestEast);
+
+        private const int _maxPaneLength = 500;
+        private const int _minPaneLength = 200;
+        private Point? _lastPoint;
+        private double _originalPaneLength;
+
+        private void Rectangle_OnPointerEntered(object? sender, PointerEventArgs e)
+        {
+            if (sender is Rectangle)
+            {
+                Cursor = SizeWestEastCursor;
+            }
+        }
+
+        private void Rectangle_OnPointerExited(object? sender, PointerEventArgs e)
+        {
+            if (sender is Rectangle)
+            {
+                Cursor = Cursor.Default;
+            }
+        }
+
+        private void Rectangle_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            if (sender is not Rectangle)
+            {
+                return;
+            }
+
+            if (_splitView is null)
+            {
+                _splitView = this.FindDescendantOfType<SplitView>();
+                if (_splitView is null)
+                {
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(_splitView.Name) || !_splitView.Name.Equals("PART_SplitView"))
+                {
+                    throw new Exception("The found split view does not have the correct name.");
+                }
+            }
+
+            if (!_splitView.IsPaneOpen)
+            {
+                return;
+            }
+
+            _lastPoint = e.GetPosition(null);
+            _originalPaneLength = _splitView.OpenPaneLength;
+            e.Handled = true;
+            e.PreventGestureRecognition();
+        }
+
+        private void Rectangle_OnPointerMoved(object? sender, PointerEventArgs e)
+        {
+            if (!_lastPoint.HasValue || sender is not Rectangle)
+            {
+                return;
+            }
+
+            if (_splitView is null || !_splitView.IsPaneOpen)
+            {
+                return;
+            }
+
+            Point mouseMovement = (Point)(e.GetPosition(null) - _lastPoint);
+            _splitView.OpenPaneLength = Math.Max(Math.Min(_originalPaneLength + mouseMovement.X, _maxPaneLength), _minPaneLength);
+        }
+
+        private void Rectangle_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            if (!_lastPoint.HasValue || sender is not Rectangle)
+            {
+                return;
+            }
+
+            _lastPoint = null;
+            _originalPaneLength = 0;
+            e.Handled = true;
+        }
+
+        #endregion
     }
 }
