156e92778876f20f7f425f66e1a909a4866e444e
BobLd <38405645+BobLd@users.noreply.github.com>
2024-11-15 19:55:05 +0000

    Tidy up and seal classes

--- Caly.Core/Controls/PdfDocumentsTabsControl.axaml.cs
+++ Caly.Core/Controls/PdfDocumentsTabsControl.axaml.cs
@@ -1,3 +1,18 @@
+// Copyright (C) 2024 BobLd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY - without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 using System;
 using System.Linq;
 using Avalonia;
@@ -8,7 +23,7 @@ using Avalonia.VisualTree;
 
 namespace Caly.Core.Controls;
 
-public partial class PdfDocumentsTabsControl : UserControl
+public sealed partial class PdfDocumentsTabsControl : UserControl
 {
     private static readonly Cursor SizeWestEastCursor = new(StandardCursorType.SizeWestEast);
 
--- Caly.Core/Controls/PdfSearchPanelControl.axaml.cs
+++ Caly.Core/Controls/PdfSearchPanelControl.axaml.cs
@@ -1,3 +1,18 @@
+// Copyright (C) 2024 BobLd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY - without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
@@ -11,7 +26,7 @@ using Caly.Core.ViewModels;
 namespace Caly.Core.Controls;
 
 [TemplatePart("PART_TextBoxSearch", typeof(TextBox))]
-public class PdfSearchPanelControl : TemplatedControl
+public sealed class PdfSearchPanelControl : TemplatedControl
 {
     private TextBox? _textBoxSearch;
 
