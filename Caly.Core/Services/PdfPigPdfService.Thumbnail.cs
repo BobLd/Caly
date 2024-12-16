e46c000b6b371e91ab28f7ecf87ce534dc789664
BobLd <38405645+BobLd@users.noreply.github.com>
2024-12-16 20:07:08 +0000

    Ensure Thumbnail rendering can be properly cancelled

--- Caly.Core/Services/PdfPigPdfService.Thumbnail.cs
+++ Caly.Core/Services/PdfPigPdfService.Thumbnail.cs
@@ -16,6 +16,7 @@
 using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
 using Caly.Core.ViewModels;
@@ -27,8 +28,10 @@ namespace Caly.Core.Services
     {
         private readonly ConcurrentDictionary<int, PdfPageViewModel> _bitmaps = new();
         
-        private async Task SetThumbnail(PdfPageViewModel vm, SKPicture picture)
+        private async Task SetThumbnail(PdfPageViewModel vm, SKPicture picture, CancellationToken token)
         {
+            token.ThrowIfCancellationRequested();
+
             int tWidth = (int)(vm.ThumbnailWidth / 1.5);
             int tHeight = (int)(vm.ThumbnailHeight / 1.5);
 
@@ -43,6 +46,8 @@ namespace Caly.Core.Services
             using (SKBitmap bitmap = new SKBitmap(tWidth, tHeight))
             using (SKCanvas canvas = new SKCanvas(bitmap))
             {
+                token.ThrowIfCancellationRequested();
+
                 canvas.Clear(SKColors.White);
                 canvas.DrawPicture(picture, ref scale);
 
